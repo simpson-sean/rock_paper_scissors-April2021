a7004f71cdaff0943115a3a1a8c0f34fa80b4161
Missael Ortiz <ortiz.missael80@gmail.com>
2021-05-07 15:37:45 -0700

    refactired and finished!

--- utils.js
+++ utils.js
@@ -1,13 +1,21 @@
-export function getRandomThrow () {
-    const randomNumber = Math.ceil(Math.random() * 3);
-        if (randomNumber === 1) {
-            return 'rock';
-        }
-        if (randomNumber === 2) {
-            return 'paper';
-        }
-        if (randomNumber === 3) {
-            return 'scissors';
-        }
-};
+export function getComputerThrow(randomNumber) {
+    if (randomNumber === 1) {
+        return 'rock';
+    }
+    if (randomNumber === 2) {
+        return 'paper';
+    }
+    if (randomNumber === 3) {
+        return 'scissors';
+    }
+}
 
+export function doesUserWin(userInput, cpuInput) {
+    if ((userInput === 'rock' && cpuInput === 'scissors') || (userInput === 'paper' && cpuInput === 'rock') || (userInput === 'scissors' && cpuInput === 'paper')) {
+        return 'win';
+    } else if (userInput === cpuInput) {
+        return 'draw';
+    } else {
+        return 'lose';
+    }
+}
